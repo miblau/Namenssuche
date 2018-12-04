501622c040ccca6409ca0452eb2425bef41a5182
miblau <michelle.blau@gmx.net>
2018-12-04 20:11:17 +0100

    Klasse "VornamensZuordnung" implementiert jetzt Serializable-Interface

--- src/main/java/model/VornamensZuordnung.java
+++ src/main/java/model/VornamensZuordnung.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
@@ -9,7 +10,7 @@ import java.util.ArrayList;
  * @author Michelle Blau
  */
 
-public class VornamensZuordnung {
+public class VornamensZuordnung implements Serializable {
     private String nachname;
     private ArrayList<String> vornamen;
 
